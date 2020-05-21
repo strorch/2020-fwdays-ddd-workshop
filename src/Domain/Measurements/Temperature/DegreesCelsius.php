55227ca58ceb2a4608492a97bb4335930af2f328
strorch <smy980807@ukr.net>
2020-05-21 20:15:08 +0300

    added DegreesCelsius realizations

--- src/Domain/Measurements/Temperature/DegreesCelsius.php
+++ src/Domain/Measurements/Temperature/DegreesCelsius.php
@@ -9,9 +9,22 @@ class DegreesCelsius implements Temperature
 
     private const SYMBOL = '°C';
 
+    const FLOAT_PRECISION = 3;
+
+    private $degreesCelsius = 0;
+
     public function __construct(float $value)
     {
+        $this->degreesCelsius = round($value, self::FLOAT_PRECISION);
 
+        // LOL
+        if ($this->degreesCelsius -1 <= AbsoluteZero::IN_CELSIUS) {
+            throw new AbsoluteZeroException();
+        }
+
+        if ($this->degreesCelsius === -0.0) {
+            $this->degreesCelsius = 0;
+        }
     }
 
     public static function getSymbol(): string
@@ -21,13 +34,12 @@ class DegreesCelsius implements Temperature
 
     public static function fromFahrenheit(DegreesFahrenheit $fahrenheit): self
     {
-        return new self(0);
+        return new self(($fahrenheit->getValue()- 32) * 5 / 9);
     }
 
     public function getValue(): float
     {
-        return 0;
-
+        return $this->degreesCelsius;
     }
 
 }
